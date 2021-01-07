4de97f3b285ed56f0519a42df34554eb4573bab6
elenh <yisiwings@163.com>
2021-01-07 15:49:08 +0800

    update: default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,38 +1,58 @@
 <template>
   <v-app>
-    <v-system-bar app>
-      <v-spacer></v-spacer>
+    <v-app-bar class="app-bar" app extended>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ $t('home.title') }}</v-toolbar-title>
 
-      <v-icon>mdi-square</v-icon>
-      <v-icon>mdi-circle</v-icon>
-      <v-icon>mdi-triangle</v-icon>
+      <div class="app-bar__right">
+        <template v-if="!isMobile">
+          <v-btn icon><v-icon>mdi-account-outline</v-icon></v-btn>
+          <v-btn icon>
+            <v-badge color="pink" content="2" overlap>
+              <v-icon>mdi-heart-outline</v-icon></v-badge
+            >
+          </v-btn>
 
-      <!-- locale lang switch menu -->
-      <v-menu bottom left>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn dark icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-translate</v-icon>
+          <v-btn icon>
+            <v-badge color="green" content="6" overlap>
+              <v-icon>mdi-cart-outline</v-icon>
+            </v-badge>
           </v-btn>
         </template>
 
-        <v-list>
-          <v-list-item v-for="(lang, i) in locales" :key="i">
-            <v-list-item-title @click="changeLocale(lang)">
-              <v-btn text block>{{ lang }}</v-btn>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-system-bar>
+        <!-- locale lang switch menu -->
+        <v-menu bottom left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark icon v-bind="attrs" v-on="on">
+              <v-icon small>mdi-translate</v-icon>
+            </v-btn>
+          </template>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-list>
+            <v-list-item v-for="(lang, i) in locales" :key="i">
+              <v-list-item-title @click="changeLocale(lang)">
+                <v-btn text block>{{ lang }}</v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
 
-      <v-toolbar-title>{{ $t('home.title') }}</v-toolbar-title>
+      <template #extension>
+        <v-text-field
+          v-model="message3"
+          class="app-bar__right__search"
+          prepend-inner-icon="search"
+          solo
+          label="search"
+          clearable
+          dense
+        ></v-text-field>
+      </template>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" fixed temporary>
-      <!--  -->
+      this is navigation drawer
     </v-navigation-drawer>
 
     <v-main class="grey lighten-2">
@@ -40,15 +60,48 @@
         <nuxt />
       </v-container>
     </v-main>
+
+    <v-bottom-navigation :input-value="isMobile" color="teal" grow app>
+      <v-btn>
+        <span>Account</span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+        <v-badge color="pink" content="3" overlap
+          ><v-icon>mdi-heart</v-icon></v-badge
+        >
+      </v-btn>
+
+      <v-btn>
+        <span>Cart</span>
+        <v-badge color="green" content="2" overlap
+          ><v-icon>mdi-cart</v-icon></v-badge
+        >
+      </v-btn>
+    </v-bottom-navigation>
+
+    <v-footer class="d-flex justify-center">
+      <div>this is app footer</div>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 export default {
-  data: () => ({ drawer: null }),
+  data() {
+    return {
+      drawer: null,
+      message3: '',
+    }
+  },
   computed: {
     ...mapState(['locales']),
+    isMobile() {
+      return this.$vuetify.breakpoint.mobile
+    },
   },
   methods: {
     changeLocale(newLang) {
@@ -67,3 +120,19 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.app-bar {
+  &__right {
+    height: 100%;
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    &__search {
+      width: 150px;
+    }
+  }
+}
+</style>
